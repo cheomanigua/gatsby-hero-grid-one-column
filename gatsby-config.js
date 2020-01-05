86d7c557dad8e994121bcf4537740ed4466dfc20
cheomanigua <ronero.canario@gmail.com>
2020-01-05 23:08:58 +0100

    installed gatsby-image gatsby-transformer-sharp gatsby-plugin-sharp gatsby-source-filesystem gatsby-background-image

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,17 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    { 
+      resolve: `gatsby-source-filesystem`,
+      options: {      
+        path: `${__dirname}/src/images/`,
+      } 
+    }
+  ],
 }
 
 
