102c0e4c5865473a3bd2102ff5453a2005c46c8e
cheomanigua <ronero.canario@gmail.com>
2020-01-06 00:49:22 +0100

    updated header.js and header.css after adding background-image plugin

--- src/components/header.js
+++ src/components/header.js
@@ -1,24 +1,51 @@
 import React from 'react'
+import { graphql, StaticQuery } from 'gatsby'
+import BackgroundImage from 'gatsby-background-image'
 
-const Header = () => <div className="wrapper" id="home">
-  
- <header>
-      <nav>
-        <div id="logo"><a href="#home">DIGITAL REALM</a></div>
-        <ul>
-          <li><a href="#services">Servicios</a></li>
-          <li><a href="#about">Nosotros</a></li>          
-          <li><a href="#contact">Contacto</a></li>
-        </ul>
-      </nav>
-      <div className="bg-image"></div>
-      <div className="content-wrap">
-        <h1>Desarrollo Web y Marketing</h1>
-        <h3>Da visibilidad a tu empresa, negocio o proyecto con nuestros servicios</h3>
-        <a href="#contact" className="btn">Contáctanos</a>
-      </div>
-    </header>
+const Header = ({ className }) => (
+  <StaticQuery
+    query={graphql`
+      query {
+        desktop: file(relativePath: { eq: "header.jpg" }) {
+          childImageSharp {
+            fluid(quality: 72, maxWidth: 1920) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+      `}
+    render={data => {
+      const imageData = data.desktop.childImageSharp.fluid
+      return (
+        <BackgroundImage
+          Tag="header"
+          className={className}
+          fluid={imageData}
+        >
+          <div className="wrapper" id="home">
+            <header>
+              <nav>
+                <div id="logo"><a href="#home">DIGITAL REALM</a></div>
+                <ul>
+                  <li><a href="#services">Servicios</a></li>
+                  <li><a href="#about">Nosotros</a></li>
+                  <li><a href="#contact">Contacto</a></li>
+                </ul>
+              </nav>
+              <div className="bg-image"></div>
+              <div className="content-wrap">
+                <h1>Desarrollo Web y Marketing</h1>
+                <h3>Da visibilidad a tu empresa, negocio o proyecto con nuestros servicios</h3>
+                <a href="#contact" className="btn">Contáctanos</a>
+              </div>
+            </header>
+          </div>
+        </BackgroundImage>
+      )
+    }
+    }
+  />
+)
 
-</div>
-  
 export default Header;
