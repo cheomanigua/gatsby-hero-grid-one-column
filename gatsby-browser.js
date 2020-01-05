2652fa3f9f8a4f34a083b685ad54bcfc2c2fae0c
cheomanigua <ronero.canario@gmail.com>
2020-01-05 22:35:00 +0100

    updated gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,9 @@
 exports.onClientEntry = () => {
+
+  // Make the navbar solid when scrolling down
   window.onscroll = () => {
     if (window.pageYOffset > 150) {
-      document.querySelector('nav').style.background = "#333";
+      document.querySelector('nav').style.background = "var(--color-auxiliary)";
       document.querySelector('nav').style.transition = "1s";
     } else {
       document.querySelector('nav').style.background = "transparent";
