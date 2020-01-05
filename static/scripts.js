6aec74e56a2098943ac9ee5d9e8777a6d743fd36
cheomanigua <ronero.canario@gmail.com>
2020-01-05 20:28:04 +0100

    deleted scripts.js

--- static/scripts.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = () => {
-  if (window.pageYOffset > 150) {
-    document.querySelector('nav').style.background = "#333";
-    document.querySelector('nav').style.transition = "1s";
-  } else {
-    document.querySelector('nav').style.background = "transparent";
-  }
-}
