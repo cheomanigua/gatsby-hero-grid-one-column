12948c573e1f997dd746b9f8909515ecab260341
cheomanigua <ronero.canario@gmail.com>
2020-01-07 00:45:53 +0100

    added SEO.js and updated layout.js with SEO component

---
+++ src/components/SEO.js
@@ -0,0 +1,64 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+import PropTypes from "prop-types"
+import { StaticQuery, graphql } from "gatsby"
+
+const SEO = ({ title, description, url}) => (
+  <StaticQuery
+    query={query}
+    render={({
+      site: {
+        siteMetadata: {
+          defaultTitle,
+          defaultDescription,
+          siteUrl,
+        },
+      },
+    }) => {
+      const seo = {
+        title: title || defaultTitle,
+        description: description || defaultDescription,
+      }
+
+      return (
+        <>
+          <Helmet title={seo.title}>
+            <meta name="description" content={seo.description} />
+            {seo.url && <meta property="og:url" content={seo.url} />}
+            )}
+            {seo.title && <meta property="og:title" content={seo.title} />}
+            {seo.description && (
+              <meta property="og:description" content={seo.description} />
+            )}
+          </Helmet>
+        </>
+      )
+    }}
+  />
+)
+
+export default SEO
+
+SEO.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  url: PropTypes.string,
+}
+
+SEO.defaultProps = {
+  title: null,
+  description: null,
+  url: null,
+}
+
+const query = graphql`
+  query SEO {
+    site {
+      siteMetadata {
+        defaultTitle: title
+        defaultDescription: description
+        siteUrl: url
+      }
+    }
+  }
+`
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Helmet} from 'react-helmet'
 import Header from './header'
+import SEO from './SEO'
 import Footer from './footer'
 import '../css/style.css'
 import '../css/header.css'
@@ -11,6 +12,7 @@ import '../css/contact.css'
 
 const Layout = ({children}) => (
   <div>
+    <SEO />
     <Helmet>
       <script src="https://use.fontawesome.com/88991bc059.js"></script>
       <script src="https://kit.fontawesome.com/ca56e255a4.js" crossorigin="anonymous"></script>
