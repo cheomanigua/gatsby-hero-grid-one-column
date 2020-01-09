c1f39b77dc2fe8da9c8ac4481abceedad6b8c068
cheomanigua <ronero.canario@gmail.com>
2020-01-09 22:36:50 +0100

    removed font-awesome links from layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Helmet} from 'react-helmet'
+// import {Helmet} from 'react-helmet'
 import Header from './header'
 import SEO from './SEO'
 import Footer from './footer'
