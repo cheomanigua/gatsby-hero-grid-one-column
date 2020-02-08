b55b1899498a799f6032b01e85c1e7c782737762
Sergio Sanchez <ronero.canario@gmail.com>
2020-02-08 22:10:35 +0100

    updated nav layout

--- src/components/header.js
+++ src/components/header.js
@@ -23,24 +23,22 @@ const Header = ({ className }) => (
           className={className}
           fluid={imageData}
         >
-          <div className="wrapper" id="home">
-            <header>
-              <nav>
-                <div id="logo"><a href="#home">DIGITAL REALM</a></div>
-                <ul>
-                  <li><a href="#services">Servicios</a></li>
-                  <li><a href="#about">Nosotros</a></li>
-                  <li><a href="#contact">Contacto</a></li>
-                </ul>
-              </nav>
-              <div className="bg-image"></div>
-              <div className="content-wrap">
-                <h1>Desarrollo Web y Marketing</h1>
-                <h3>Da visibilidad a tu empresa, negocio o proyecto con nuestros servicios</h3>
-                <a href="#contact" className="btn">Contáctanos</a>
-              </div>
-            </header>
-          </div>
+          <header id="home">
+            <nav>
+              <div id="logo"><a href="#home">DIGITAL REALM</a></div>
+              <ul>
+                <li><a href="#services">Servicios</a></li>
+                <li><a href="#about">Nosotros</a></li>
+                <li><a href="#contact">Contacto</a></li>
+              </ul>
+            </nav>
+            <div className="bg-image"></div>
+            <div className="content-wrap">
+              <h1>Desarrollo Web y Marketing</h1>
+              <h3>Da visibilidad a tu empresa, negocio o proyecto con nuestros servicios</h3>
+              <a href="#contact" className="btn">Contáctanos</a>
+            </div>
+          </header>
         </BackgroundImage>
       )
     }
