06bb9af7df773a90d15c7096402d0795407f49cf
Sergio Sanchez <ronero.canario@gmail.com>
2020-02-08 22:40:31 +0100

    updated about.js with actual content

--- src/sections/about.js
+++ src/sections/about.js
@@ -34,15 +34,15 @@ const About = () => (
     render={data => (
       <section id="about">        
         <div className="twoColumns">
-          <div className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ea maxime animi incidunt quisquam ducimus enim. Quod illo quo quas?</div>
+          <div className="text">Diseño "responsive" para que la navegación y experiencia del usuario con la interfaz sea siempre perfecta independientemente del dispositivo utilizado</div>
           <div className="illustration"><Img fluid={data.mobile.childImageSharp.fluid} alt="Mobile" /></div>
         </div>
         <div className="twoColumns">
           <div className="illustration"><Img fluid={data.charts.childImageSharp.fluid} alt="Charts" /></div>
-          <div className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ea maxime animi incidunt quisquam ducimus enim. Quod illo quo quas?</div>
+          <div className="text">Conseguimos las puntuaciones con los valores máximos en las pruebas de rendimiento y SEO de stándares como Lighthouse</div>
         </div>
         <div className="twoColumns">
-          <div className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ea maxime animi incidunt quisquam ducimus enim. Quod illo quo quas?</div>
+          <div className="text">Ofrecemos formación para el mantenimiento e implementación de nuevas funcionalidades de tu sitio web en el futuro</div>
           <div className="illustration"><Img fluid={data.teamcollab.childImageSharp.fluid} alt="Collaboration" /></div>
         </div>
       </section>
